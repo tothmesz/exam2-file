cd63af4e657dfdc4f823a50c3cf948bee31ee87b
tothmesz <66495712+tothmesz@users.noreply.github.com>
2021-03-24 17:13:12 +0100

    finished

--- src/main/java/com/codecool/chessopen/ChessResults.java
+++ src/main/java/com/codecool/chessopen/ChessResults.java
@@ -1,20 +1,38 @@
 package com.codecool.chessopen;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.io.*;
+import java.util.*;
+import java.util.stream.Stream;
 
 public class ChessResults {
 
-    public List<String> getCompetitorsNamesFromFile(String fileName){
-        //TODO your code comes here
-        return null;
+    public List<String> getCompetitorsNamesFromFile(String fileName) {
+        List<String> result = new ArrayList<>();
+        Map competitors = new HashMap();
+        File file = new File(fileName);
+        try(BufferedReader reader = new BufferedReader(new FileReader(file))) {
+            String currentLine = null;
+            while ((currentLine = reader.readLine()) != null) {
+                List<String> myList = new ArrayList<String>(Arrays.asList(currentLine.split(",")));
+                int countPoints = 0;
+                for (int i = 1; i < myList.size(); i++) {
+                    countPoints += Integer.parseInt(myList.get(i));
+                }
+                competitors.put(myList.get(0), countPoints);
+            }
+            Stream set = competitors.entrySet().stream().sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()));
+            Iterator itr = set.iterator();
+            while(itr.hasNext()) {
+                Map.Entry entry = (Map.Entry) itr.next();
+                String entryToAdd = (String) entry.getKey();
+                result.add(entryToAdd);
+            }
+
+
+        } catch (IOException e) {
+            System.out.println("File not found!");
+        }
+        return result;
     }
 
 }
